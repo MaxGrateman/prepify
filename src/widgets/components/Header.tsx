f314deb6ca18148a627ff9dc64b5710567228506
Shakhrukh Abdugapar <69681238+shahruhabd@users.noreply.github.com>
2025-01-21 23:02:23 +0500

    Update Header.tsx

--- src/widgets/components/Header.tsx
+++ src/widgets/components/Header.tsx
@@ -50,17 +50,17 @@ function Header() {
                                                      after:bg-white after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%] text-base">
                         <span>COURSES</span>
                     </Link>
-                    <Link href="/about" className="relative transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-white 
+                    <Link href="#" className="relative transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-white 
                                                     before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-200 after:absolute
                                                      after:bg-white after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%] text-base">
                         <span>ABOUT US</span>
                     </Link>
-                    <Link href="/contacts" className="relative transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-white 
+                    <Link href="#" className="relative transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-white 
                                                     before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-200 after:absolute
                                                      after:bg-white after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%] text-base">
                         <span>CONTACT</span>
                     </Link>
-                    <Link href="/support" className="relative transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-white 
+                    <Link href="#" className="relative transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-white 
                                                     before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-200 after:absolute
                                                      after:bg-white after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%] text-base">
                         <span>SUPPORT</span>
