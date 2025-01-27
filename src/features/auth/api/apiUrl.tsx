94d98a60ef484519fb581dff47f72900faf8264a
MaxGrateman <grekf2@mail.ru>
2025-01-27 14:53:00 +0500

    Add api links & proile

--- src/features/auth/api/apiUrl.tsx
+++ src/features/auth/api/apiUrl.tsx
@@ -1,5 +1,5 @@
 {/*Компонент с айпишками для регистрации и логина*/}
 
-export const apiPasswordRecover = 'http://157.245.123.144/api/password/forgot';
-export const apiRegistration = 'http://157.245.123.144/api/auth/register';
-export const apiLogin = 'http://157.245.123.144/api/auth/login';
+export const apiPasswordRecover = 'http://157.245.123.144:8088/api/password/forgot';
+export const apiRegistration = 'http://157.245.123.144:8088/api/auth/register';
+export const apiLogin = 'http://157.245.123.144:8088/api/auth/login';
--- src/features/auth/components/AuthForm.tsx
+++ src/features/auth/components/AuthForm.tsx
@@ -218,7 +218,7 @@ function AuthForm({ isRegister = false, apiUrl }: AuthFormProps) {
         // </section>
 
         <section className="mt-48">
-            <form className="max-w-md mx-auto border border-neutral-100 shadow-md shadow-neutral-100/50 rounded-md box-border p-8">
+            <form onSubmit={handleSubmit} className="max-w-md mx-auto border border-neutral-100 shadow-md shadow-neutral-100/50 rounded-md box-border p-8">
                 <h3 className="mb-6 text-xl">{isRegister ? 'REGISTRATION' : 'LOGIN'}</h3>
 
                 {isRegister && (
@@ -266,15 +266,15 @@ function AuthForm({ isRegister = false, apiUrl }: AuthFormProps) {
                 {isRegister && (
                     <div className="relative z-0 w-full mb-5 group">
                         <input type="password" 
-                            name="passwordConfirmation" 
-                            id="passwordConfirmation" 
+                            name="password_confirmation" 
+                            id="password_confirmation" 
                             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
                             placeholder=" " 
                             required
                             autoComplete="new-password"
                             value={formData.password_confirmation}
                             onChange={handleChange} />
-                        <label htmlFor="passwordConfirmation" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+                        <label htmlFor="password_confirmation" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
                     </div>
                 )}
             
