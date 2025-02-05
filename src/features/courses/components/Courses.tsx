a1718d74ca5481557908773437fefec15a05ee11
MaxGrateman <grekf2@mail.ru>
2025-02-05 09:51:08 +0500

    Add profile & course errors

--- src/features/courses/components/Courses.tsx
+++ src/features/courses/components/Courses.tsx
@@ -3,11 +3,12 @@
 import {useRouter} from "next/navigation";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "@/lib/store";
-import {SetStateAction, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {fetchCourses} from "@/lib/features/courses/coursSlice";
 import ModalCourse from "@/widgets/components/modalCourse";
 import { motion } from "motion/react";
-import Loading from "@/widgets/components/Loading";
+
+{/*Интерфейс карточки-курса*/}
 interface Course {
     id: number,
     name: string | null,
@@ -15,8 +16,6 @@ interface Course {
 }
 
 export default function Courses() {
-    const dispatch = useDispatch<AppDispatch>();
-
     const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
     const [showModal, setShowModal] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
@@ -25,6 +24,8 @@ export default function Courses() {
     const handleMouseEnter = (id: number) => setHoveredId(id);
     const handleMouseLeave = () => setHoveredId(null);
 
+    {/*Обработка запроса курсов*/}
+    const dispatch = useDispatch<AppDispatch>();
     const user = useSelector((state: RootState) => state.user);
     const {courses, loading, error} = useSelector((state: RootState) => state.courses);
     const router = useRouter();
@@ -38,6 +39,7 @@ export default function Courses() {
         setSelectedCourse(null); 
     }
 
+    {/*Кнопка с проверкой на юзера, на каждой карточке*/}
     const handleButtonClick = (course: Course) => {
         if (!user.user) {
             setErrorMessage('Please, log in to continue');
@@ -58,6 +60,8 @@ export default function Courses() {
         <section className="mt-10 px-8">
             <div className="flex items-start flex-wrap gap-5">
                 <h1 className="text-3xl basis-1/6"><span className="text-5xl text-blue-600">.</span>COURSES</h1>
+
+                {/*Поисковая строка*/}
                 <form className="mt-4 basis-2/3">
                     <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-1 pointer-events-none">
@@ -76,59 +80,73 @@ export default function Courses() {
                                 required />
                     </div>
                 </form>
+
+                {/*Фильтры*/}
                 <div className="basis-1/6 w-80 h-96 box-border p-4 border-neutral-100 shadow-sm shadow-neutral-100/50 border rounded-lg flex justify-items-start">
                     <h2 className="text-2xl">FILTERS</h2>
                 </div>
-                <div className="basis-2/3 grid grid-cols-3 gap-4 place-items-start">
-                {loading || error &&
-                Array.from({ length: 15 }).map((_, index) => (
-                    <div
-                        key={index}
-                        className="w-full h-auto box-border p-8 bg-gray-300 rounded-lg sm:w-96 dark:bg-gray-700"
-                    >   <div className="flex flex-col items-start justify-center animate-pulse">
-                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 "></div>
-                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-40 mb-2.5"></div>
-                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 mb-2.5"></div>
-                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-40 mb-2.5"></div>
-                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-40 mb-2.5"></div>
-                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-36"></div>
-                        </div>
-                    </div>
-                ))}
 
-                    {courses.map((course, index) => (
-                        <motion.div
-                        key={course.id}
-                        className="w-full h-auto box-border p-8 border-neutral-100 shadow-sm shadow-neutral-100/50 border rounded-lg"
-                        initial={false}
-                        animate={{
-                            scale: hoveredId === course.id ? 1.05 : hoveredId ? 0.95 : 1,
-                            x:
-                            hoveredId === course.id
-                                ? 0
-                                : hoveredId !== null
-                                ? Math.sign(index - courses.findIndex((c) => c.id === hoveredId)) * 5
-                                : 0,
-                        }}
-                        transition={{ type: "spring", stiffness: 300, damping: 20 }}
-                        onMouseEnter={() => handleMouseEnter(course.id)}
-                        onMouseLeave={handleMouseLeave}
-                        >
-                        <h5 className="text-lg font-bold">{course.name ?? "Default name"}</h5>
-                        <p className="text-sm font-semibold text-gray-700">{course.description ?? "Default description"}</p>
-                        <button className="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg 
-                                        hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                    type="button"
-                                    onClick={() => handleButtonClick(course)}
-                                >
-                                Start
-                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                </svg>
-                        </button>
-                        </motion.div>
-                    ))}
+                {/*Вывод карточек-курсов с обработчиками загрузки и ошибок*/}
+                <div className="basis-2/3 grid grid-cols-3 gap-4 place-items-start">
+                    {loading ? (
+                        Array.from({ length: 15 }).map((_, index) => (
+                            <div
+                                key={index}
+                                className="w-full h-auto box-border p-8 bg-gray-300 rounded-lg sm:w-96 dark:bg-gray-700"
+                            >
+                                <div className="flex flex-col items-start justify-center animate-pulse">
+                                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 "></div>
+                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-40 mb-2.5"></div>
+                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 mb-2.5"></div>
+                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-40 mb-2.5"></div>
+                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-40 mb-2.5"></div>
+                                    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-500 w-36"></div>
+                                </div>
+                            </div>
+                        ))
+                    ) : error ? (
+                        <div className="text-red-600 bg-red-100 p-4 rounded-lg sm:w-96 dark:bg-red-900 dark:text-red-300">
+                            <p className="font-medium tracking-wider">Sorry, something went wrong!</p>
+                            <p>{error}</p>
+                        </div>
+                    ) : (
+                        courses.map((course, index) => (
+                            <motion.div
+                            key={course.id}
+                            className="w-full h-auto box-border p-8 border-neutral-100 shadow-sm shadow-neutral-100/50 border rounded-lg"
+                            initial={false}
+                            animate={{
+                                scale: hoveredId === course.id ? 1.05 : hoveredId ? 0.95 : 1,
+                                x:
+                                hoveredId === course.id
+                                    ? 0
+                                    : hoveredId !== null
+                                    ? Math.sign(index - courses.findIndex((c) => c.id === hoveredId)) * 5
+                                    : 0,
+                            }}
+                            transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                            onMouseEnter={() => handleMouseEnter(course.id)}
+                            onMouseLeave={handleMouseLeave}
+                            >
+                            <h5 className="text-lg font-bold">{course.name ?? "Default name"}</h5>
+                            <p className="text-sm font-semibold text-gray-700">{course.description ?? "Default description"}</p>
+                            <button className="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg 
+                                            hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                        type="button"
+                                        onClick={() => handleButtonClick(course)}
+                                    >
+                                    Start
+                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                    </svg>
+                            </button>
+                            </motion.div>
+                        ))
+                    )}
                 </div>
+
+
+                {/*Скрытая модалка курсов*/}
                 {selectedCourse && (
                         <ModalCourse id={selectedCourse.id}
                                      name={selectedCourse.name}
--- src/features/profile/components/Profile.tsx
+++ src/features/profile/components/Profile.tsx
@@ -41,75 +41,39 @@ const Profile: React.FC<ProfileProps> = ({ userId }) => {
 
     return(
         <>
-            <div className="bg-transparent shadow-xl border-b border-white pb-8">
+            <div className="bg-transparent shadow-xl h-1/2 pb-8 mx-4">
                 <div x-data="{ openSettings: false }" className="absolute right-12 mt-4 rounded">
-                    <button className="border border-gray-400 p-2 rounded text-gray-300 hover:text-gray-300 bg-gray-100 bg-opacity-10 hover:bg-opacity-20" title="Settings">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
-                        </svg>
-                    </button>
-                    <div x-show="openSettings"className="bg-white absolute right-0 w-40 py-2 mt-1 border border-gray-200 shadow-2xl" style={{display: 'none'}}>
-                        <div className="py-2 border-b">
-                            <p className="text-gray-400 text-xs px-6 uppercase mb-1">Settings</p>
-                            <button className="w-full flex items-center px-6 py-1.5 space-x-2 hover:bg-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
+                </div>
+                <div className='mt-20 max-w-md border border-neutral-100 shadow-lg shadow-neutral-100/50 rounded-md box-border p-8 h-full'>
+                    <div className="flex flex-col items-center -mt-20 z-20 gap-1 relative">
+                        <Image src={user?.image_path as string} alt="profile_avatar" width={100} height={100} className='w-40 h-40 border-2 border-neutral-100 shadow-lg shadow-neutral-100/50 rounded-full object-cover'/>
+                        <div className="flex items-center space-x-2 my-3">
+                            <p className="text-2xl">{user?.name}</p>
+                            <span className="bg-blue-500 rounded-full p-1" title="Verified">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-100 h-2.5 w-2.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
                                 </svg>
-                                <span className="text-sm text-gray-700">Share Profile</span>
+                            </span>
+                        </div>
+                        <p className="text-gray-600 font-medium tracking-wider">Email: <span className='text-blue-700'>{user?.email}</span></p>
+                        <p className="text-lg font-medium tracking-wider text-gray-500">Level: <span className='text-blue-700'>{user?.level}</span></p>
+                        <p className="text-lg font-medium tracking-wider text-gray-500">Stack: <span className='text-blue-700'>{user?.stack}</span></p>
+                        <p className="text-lg font-medium tracking-wider text-gray-500">Country: <span className='text-blue-700'>{user?.country}</span></p>
+                        <p className="text-lg font-medium tracking-wider text-gray-500">About: <span className='text-blue-700'>{user?.about}</span></p>
+                        <div className="flex-1 flex flex-col items-center px-8 mt-2">
+                        <div className="absolute -bottom-[105px] flex items-center space-x-4">
+                            <button className="transition ease-in-out duration-300 mt-5 italic text-xl self-end font-medium
+                                    text-white bg-primary-black border border-white hover:bg-white hover:text-black focus:outline-none focus:ring-1 focus:ring-white
+                                    rounded-3xl px-4 me-2 mb-2 shadow-md shadow-neutral-100/50">
+                            EDIT
                             </button>
-                            <button className="w-full flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>
-                                </svg>
-                                <span className="text-sm text-gray-700">Block User</span>
-                            </button>
-                            <button className="w-full flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
-                                <span className="text-sm text-gray-700">More Info</span>
+                            <button className="transition ease-in-out duration-300 mt-5 italic text-xl self-end font-medium
+                                    text-white bg-primary-black border border-white hover:bg-white hover:text-black focus:outline-none focus:ring-1 focus:ring-white
+                                    rounded-3xl px-4 me-2 mb-2 shadow-md shadow-neutral-100/50">
+                            SAVE
                             </button>
                         </div>
-                        <div className="py-2">
-                            <p className="text-gray-400 text-xs px-6 uppercase mb-1">Feedback</p>
-                            <button className="w-full flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
-                                </svg>
-                                <span className="text-sm text-gray-700">Report</span>
-                            </button>
                         </div>
-                    </div>
-                </div>
-                <div className="w-full h-[250px] relative">
-                </div>
-                <div className="flex flex-col items-center -mt-20 z-20">
-                    <Image src={user?.image_path as string} alt="profile_avatar" width={100} height={100} className='w-40 h-40 border-4 border-white rounded-full object-cover'/>
-                    <div className="flex items-center space-x-2 mt-2">
-                        <p className="text-2xl">{user?.name}</p>
-                        <span className="bg-blue-500 rounded-full p-1" title="Verified">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-100 h-2.5 w-2.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
-                            </svg>
-                        </span>
-                    </div>
-                    <p className="text-gray-700">{user?.level}</p>
-                    <p className="text-sm text-gray-500">{user?.country}</p>
-                </div>
-                <div className="flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2">
-                    <div className="flex items-center space-x-4 mt-2">
-                        <button className="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path>
-                            </svg>
-                            <span>Connect</span>
-                        </button>
-                        <button className="flex items-center bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd"></path>
-                            </svg>
-                            <span>Message</span>
-                        </button>
                     </div>
                 </div>
             </div>
